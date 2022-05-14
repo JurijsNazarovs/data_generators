9cd32fd21e2857095b60ff326334811b6937edc4
JurijsNazarovs <nazarovs@wisc.edu>
2022-05-13 21:29:50 -0500

    add visualization

--- ntu_rbgd_skeleton/ntu_rgbd.py
+++ ntu_rbgd_skeleton/ntu_rgbd.py
@@ -4,31 +4,45 @@ import os
 import torch
 import numpy as np
 
+import matplotlib
+# matplotlib.use('TkAgg')
+matplotlib.use('Agg')
+import matplotlib.pyplot
+import matplotlib.pyplot as plt
+from PIL import Image
+import io
+
 from torch.utils.data import Dataset
 
+
 class NTU_RGBD(Dataset):
-    def __init__(self, h5_file, keys=None, max_len=50):
+    def __init__(self, h5_file, keys=None, max_len=50, batch_size=1):
+        # Keys are actions
         assert os.path.exists(h5_file), f"HDF5 file {h5_file} does not exist"
         self._h5_file_handle = h5.File(h5_file, 'r')
-        self.keys = keys if keys is not None else list(self._h5_file_handle.keys())
+        self.keys = keys if keys is not None else list(
+            self._h5_file_handle.keys())
         self.max_len = max_len
         data_seq_len = self._h5_file_handle[f"{self.keys[0]}/pose"].shape[1]
         self.t = torch.linspace(0., 1., min(data_seq_len, self.max_len))
-        
+
         self._key_start_idx = np.zeros(len(self.keys), dtype=np.int64)
-        sum = 0
+        n_data = 0
+
         for i, k in enumerate(self.keys):
-            self._key_start_idx[i] = sum
-            sum += self._h5_file_handle[k].attrs['len']
-        self.max_idx = sum
-    
+            self._key_start_idx[i] = n_data
+            n_data += self._h5_file_handle[k].attrs['len']
+        self.max_idx = n_data
+        self.batch_size = batch_size
+        self.n_batches = int(np.ceil(self.max_idx / self.batch_size))
+
     def __getitem__(self, index):
         action, idx = self.get_action_idx(index)
-        
-        pose = torch.tensor(self._h5_file_handle[f"{action}/pose"][idx, :self.max_len])
 
+        pose = torch.tensor(
+            self._h5_file_handle[f"{action}/pose"][idx, :self.max_len])
         return pose
-    
+
     def __len__(self):
         return self.max_idx
 
@@ -38,11 +52,200 @@ class NTU_RGBD(Dataset):
         action = self.keys[action_idx]
         idx = _sub[action_idx]
         return action, idx
-    
+
     def close(self):
         self._h5_file_handle.close()
-    
+
+    def visualize(
+            self,
+            traj,
+            concatenate=True,
+            plot_path=None,  #'plots/rotmnist/traj',
+            img_w=None,
+            img_h=None,
+            save_separate=False,
+            add_frame=True,
+            **kwargs):
+        T = len(traj)
+        try:
+            # In case of tensor
+            traj = traj.cpu().numpy()
+        except:
+            pass
+
+        # traj = (traj * (self.data_max - self.data_min) +\
+        #   self.data_min)
+
+        def save_image(data, filename):
+
+            im = Image.fromarray(np.transpose(data, (1, 0, 2)))
+            #im = ImageOps.flip(im)
+            if img_w is not None and img_h is not None:
+                n_pics = im.size[0] // im.size[1]  #w//h
+                im = im.resize((img_w * n_pics, img_h))
+            im.save(filename)
+
+        def resize_array(data):
+
+            if img_w is not None and img_h is not None:
+                if len(data.shape) == 2:
+                    # Grey scale
+                    im = Image.fromarray(np.transpose(data))
+                else:
+                    im = Image.fromarray(np.transpose(data, (1, 0, 2)))
+                n_pics = im.size[0] // im.size[1]  #w//h
+                im = im.resize((img_w * n_pics, img_h))
+                if len(data.shape) == 2:
+                    # Grey scale
+                    resized_data = np.transpose(
+                        self.arr_from_img(im)[0].astype(np.uint8))
+                else:
+                    resized_data = np.transpose(self.arr_from_img(im),
+                                                (2, 1, 0)).astype(np.uint8)
+                return resized_data
+            else:
+                return data
+
+        def make_frame(img):
+            img = img.astype(int)
+            color_placement = -128
+            width = int(0.01 * img.shape[1])
+            gap = 0
+
+            img_h, img_w = img.shape[:2]
+
+            # Horizontal lines
+            col_ind = list(range(0, img_w))
+            row_ind = list(range(gap, width+gap)) +\
+                list(range(img_h - width - gap,  img_h - gap))
+            img[np.ix_(row_ind, col_ind)] = color_placement
+
+            # Vertical lines
+            col_ind = list(range(0, width))+\
+                list(range(img_w-width, img_w))
+            row_ind = list(range(gap, img_h - gap))
+            img[np.ix_(row_ind, col_ind)] = color_placement
+
+            RGB = np.zeros((img_h, img_w, 3))
+
+            if len(img.shape) < 3:
+                RGB[img != color_placement] = np.stack(
+                    [img[img != color_placement]] * 3, axis=1)
+                # Make frame different color
+                RGB[(img == color_placement)] = [131, 156, 255]
+            else:
+                RGB[img != color_placement] = img[img != color_placement]
+                # Make frame different color
+                RGB[(img == color_placement)[:, :, 0]] = [131, 156, 255]
+
+            return RGB.astype(np.uint8)  #Image.fromarray(RGB.astype(np.uint8))
+
+        if concatenate:
+            concat_image = []
+
+        if plot_path is not None:
+            os.makedirs(os.path.dirname(plot_path), exist_ok=True)
+
+        for t in range(T):
+            image = self.get_picture_array(traj[t][None], 0, 0)
+
+            if add_frame:
+                image = make_frame(image)
+            if concatenate:
+                concat_image.append(image)
+            if plot_path is not None and save_separate:
+                tmp = list(os.path.splitext(plot_path))
+                print(tmp)
+                if tmp[1] == '':
+                    # Extension
+                    tmp[1] = 'png'
+                save_image(image, tmp[0] + '_%03d.' % t + tmp[1])
+
+        if concatenate:
+            # Concatenate to 0, because we transpose images when save.
+            # Because PIL and numpy have different order of axes
+            concat_image = np.concatenate(concat_image, axis=0)
+            if plot_path is not None:
+                tmp = list(os.path.splitext(plot_path))
+                if tmp[1] == '':
+                    # Extension
+                    tmp[1] = '.png'
+                save_image(concat_image, tmp[0] + '_concat' + tmp[1])
+
+            concat_image = resize_array(concat_image)
+            return concat_image
+
+    def arr_from_img(self, im, shift=0):
+        w, h = im.size
+        arr = im.getdata()
+        #c = int(np.product(arr.size) / (w * h))
+        c = len(im.getbands())
+        return np.asarray(arr, dtype=np.float32).reshape(
+            (h, w, c)).transpose(2, 1, 0) - shift * 255
+
+    def get_picture_array(self, X, index, shift=0):
+        X = self.plot_skeleton(X[0])[None]
+
+        ch, w, h = X.shape[1], X.shape[2], X.shape[3]
+        ret = (X[index] + shift*255).\
+            reshape(ch, w, h).transpose(2, 1, 0).clip(0, 255).astype(np.uint8)
+        if ch == 1:
+            ret = ret.reshape(h, w)
+
+        return ret
+
+    def plot_skeleton(self, x):
+
+        xmax = np.max(x[0]) * 1.1
+        xmin = np.min(x[0]) * 1.1
+        ymax = np.max(x[1]) * 1.1
+        ymin = np.min(x[1]) * 1.1
+
+        x = x.transpose()  #[25, 3] -> [3, 25]
+        # Determine which nodes are connected as bones according to NTU skeleton structure
+        # Note that the sequence number starts from 0 and needs to be minus 1
+        arms = np.array([24, 12, 11, 10, 9, 21, 5, 6, 7, 8, 20]) - 1  #Arms
+        rightHand = np.array([12, 25]) - 1  #one 's right hand
+        leftHand = np.array([8, 23]) - 1  #left hand
+        legs = np.array([20, 19, 18, 17, 1, 13, 14, 15, 16]) - 1  #leg
+        body = np.array([4, 3, 21, 2, 1]) - 1  #body
+
+        color_point = 'blue'  # '#03ff'  #Joint point color
+        color_bone = 'red'  #Bone color
+
+        #fig = plt.figure()  #figsize=(.28, .28))
+        fig, ax = plt.subplots()  #figsize=(2, 2))
+        ax.axis('off')
+        #Drawing joint xs through scatter diagram
+        plt.scatter(x[0, :], x[1, :], c=color_point, s=40.0)
+        #Draw the connecting line between two xs through the line diagram,
+        #that is, the bone
+        plt.plot(x[0, arms], x[1, arms], c=color_bone, lw=2.0)
+        plt.plot(x[0, rightHand], x[1, rightHand], c=color_bone, lw=2.0)
+        plt.plot(x[0, leftHand], x[1, leftHand], c=color_bone, lw=2.0)
+        plt.plot(x[0, legs], x[1, legs], c=color_bone, lw=2.0)
+        plt.plot(x[0, body], x[1, body], c=color_bone, lw=2.0)
+
+        #plt.xlim(xmin, xmax)
+        #plt.ylim(ymin, ymax)
+        plt.xticks([], [])
+        plt.yticks([], [])
+
+        io_buf = io.BytesIO()
+        fig.savefig(io_buf, format='raw')  #, dpi=DPI)
+        io_buf.seek(0)
+        data_plot = np.reshape(np.frombuffer(io_buf.getvalue(),
+                                             dtype=np.uint8),
+                               newshape=(int(fig.bbox.bounds[3]),
+                                         int(fig.bbox.bounds[2]), -1))
+        io_buf.close()
+        data_plot = data_plot[:, :, :3].transpose(2, 0, 1)
+        return data_plot
+
+        #data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+
+
 if __name__ == "__main__":
-    dataset = NTU_RGBD("../../func_ode/data/nturgb+d.hdf5", max_len=150)
+    dataset = NTU_RGBD("./nturgb+d.hdf5", max_len=23)
     print(len(dataset.t))
-    print(dataset[500])
+    print(dataset[500].shape)
